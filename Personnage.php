0b24b7cbf794011036df7539f4a24e15df8c987c
AUBEL Luidjy <aubelui@ndlp.fr>
2021-09-09 11:57:57 +0200

    first

--- Personnage.php
+++ Personnage.php
@@ -1,14 +1,46 @@
 <?php
 
-class Personnage {
+class Personnage
+{
 // les attributs et méthode ici
-private $_nom = "Inconnu";
-private $_force = 50;
-private $_experience = 1;
-private $_degats = 0;
+    private $_nom = "Inconnu";
+    private $_force = 50;
+    private $_experience = 1;
+    private $_degats = 0;
 
-
-
+//méthode
+    public function afficherstat(){
+        print($this->_force." ");
+        print($this->_experience." ");
+        print( $this->_degats." ");
+    }
+     public function definirForce($force)
+    {
+        $this->_force = $force;
+    }
+    public function definirDegat($degat)
+    {
+        $this->_degats = $degat;
+    }
+    public function definirXp($xp)
+    {
+        $this->_experience = $xp;
+    }
+    public function parler()
+    {
+        print("Je suis un personnage<br>");
+    }
+    public function gagnerXp()
+    {
+        $this->_experience++;
+        return $this->_experience;
+    }
+    public function afficherXp(){
+        print("Experience : ".$this->_experience);
+    }
+    public function frapper($Victime)
+    {
+        $Victime->_degats = $Victime->_degats + $this->_force;
+    }
 
 }
-?>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+include "Personnage.php";
+print ("Jeu de combat<br>");
+$perso1 = new Personnage();
+$perso2 = new Personnage();
+
+$perso1->definirForce(100);
+$perso1->definirDegat(10);
+$perso1->definirXp(5);
+
+$perso2->definirForce(10);
+$perso2->definirDegat(8);
+$perso2->definirXp(2);
+
+print("perso2 stat :".$perso2->afficherstat());
+//$perso1->parler();
+//$perso1->afficherXp();
+$perso1->frapper($perso2);
+$perso2->frapper($perso1);
+print("perso2 stat :".$perso2->afficherstat());
+?>
