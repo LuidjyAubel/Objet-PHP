f0a83f8967e79ec895f0303fd30f92497cad9cf4
AUBEL Luidjy <aubelui@ndlp.fr>
2021-09-23 15:40:26 +0200

    il est vivant

--- PersonnageManager.php
+++ PersonnageManager.php
@@ -4,7 +4,7 @@ class PersonnageManager{
     public function __construct($db){
         $this->setdb($db);
     }
-    public function setdb($_db):PDO{
+    public function setdb($_db)/*:PDO*/{
         $this->_db = $_db;
     }
     public function adddb(Personnage $perso):bool
--- index2.php
+++ index2.php
@@ -1,25 +1,32 @@
 <?php
 //include "Personnage.php";
-function chargerClass($class){
- include $class.".php";
+function chargerClass($class)
+{
+    include $class . ".php";
 }
 spl_autoload_register("chargerClass");
 print("<h1>Jeu 2 Baston (la bagarre !!!!!!)</h1>");
 
 include 'conf.php';
 
-try{
+try {
     $db = new PDO($host, $user, $passw);
-    $PersonnageManager = new PersonnageManager();
-   /* $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
-    if ($db){
-        print('base de donnée');
-        $requete = $db->query('SELECT id, nom, `force`, degats, experience, niveau FROM perso;');
-        while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
-            $perso = new Personnage($ligne);
-            print('info '.$perso->getid(). " ".$perso->getnom()." ".$perso->getNiveau()." ".$perso->getForce()." ".$perso->getExperience()." ".$perso->getDegats());
-        }
-    }*/
-}catch (PDOException $e){
+    $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+    $PersonnageManager = new PersonnageManager($db);
+    $personnage = $PersonnageManager->getList();
+    print("liste des personnages : ");
+    foreach ($personnage as $personnages) {
+        print('<br>' . $personnages->getNom()." (".$personnages->getforce().") ");
+    }
+    /* $db->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+if ($db){
+print('base de donnée');
+$requete = $db->query('SELECT id, nom, `force`, degats, experience, niveau FROM perso;');
+while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
+$perso = new Personnage($ligne);
+print('info '.$perso->getid(). " ".$perso->getnom()." ".$perso->getNiveau()." ".$perso->getForce()." ".$perso->getExperience()." ".$perso->getDegats());
+}
+}*/
+} catch (PDOException $e) {
     print($e->getmessage());
 }
