0d7e185f50647d65831b115ca6fb142fa6fc7b6b
AUBEL Luidjy <aubelui@ndlp.fr>
2021-09-23 12:02:36 +0200

    efffg

--- PersonnageManager.php
+++ PersonnageManager.php
@@ -7,16 +7,30 @@ class PersonnageManager{
     public function setdb($_db):PDO{
         $this->_db = $_db;
     }
-    public function adddb(Personnage $perso):bool{
+    public function adddb(Personnage $perso):bool
+    {
 
     }
-    public function readdb(){
+    public function readdb()
+    {
 
     }
-    public function deletedb(Personnage $person){
+    public function deletedb(Personnage $person)
+    {
 
     }
-    public function updatedb(){
+    public function updatedb(Personnage $perso):bool
+    {
 
+    }
+     public function getList():array
+    {
+        $tabperso = array();
+        $requete = $this->_db->query('SELECT id, nom, `force`, degats, experience, niveau FROM perso;');
+        while ($ligne = $requete->fetch(PDO::FETCH_ASSOC)){
+            $perso = new Personnage($ligne);
+            $tabperso[] = $perso;
+        }
+        return $tabperso;
     }
 }
